8cbc275816ba38f556a2559a95accc857eeda573
Ly <514432996@qq.com>
2019-02-13 11:06:56 +0800

    update:添加快速排序功能

--- LYUtils.py
+++ LYUtils.py
@@ -3,6 +3,21 @@ LYUtils.py
 '''
 
 '''
+快速排序
+'''
+def partition(seq):
+    pi, seq = seq[0], seq[1:]
+    lo = [x for x in seq if x <= pi]
+    hi = [x for x in seq if x > pi]
+    return lo, pi, hi
+
+def quick_sort(seq):
+    if len(seq) <= 1: return seq
+    lo, pi, hi = partition(seq)
+    return quick_sort(lo) + pi + quick_sort(hi)
+
+
+'''
 Database connect
 '''
 import  pymongo
